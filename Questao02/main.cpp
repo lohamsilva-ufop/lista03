612533ad97a982c3ea54ddd0412d1f0987fd2acc
lohamsilva-ufop <loham.silva@aluno.ufop.edu.br>
2021-04-23 20:20:30 -0300

    Questao 02

--- Questao02/main.cpp
+++ Questao02/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+/*premissa: enquanto a ultima permutação tiver um elemento móvel, faça:
+encontre o maior elemento k
+troque k com o elemento adjacente
+troque a seta de todos os elementos maiores que k
+adicione a nova permutação da lista
+*/
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+void johnsonTrotter(int n){
+    while(){
+        for (i=0; i<n; i++){
+
+
+        }
+
+    }
+
+}
+
+int main(){
+    int elemento;
+
+    cout << "Informe o elemento:";
+    cin >> elemento;
+
+    johnsonTrotter(elemento);
+
     return 0;
 }
